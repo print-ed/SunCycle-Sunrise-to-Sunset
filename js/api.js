d10faa7b7f44f05d253a5fba69b2bb670ed33093
Edreyel Adora <edreyel.aadora@gmail.com>
2024-01-04 00:42:03 +0800

    bugfix: add comments and fix indentation

--- js/api.js
+++ js/api.js
@@ -2,6 +2,7 @@
 async function fetchSunriseSunsetData(lat, lng) {
   try {
     const response = await fetch(
+      // Replace with your sunrise-sunset API endpoint and key
       `https://api.sunrisesunset.io/json?lat=${lat}&lng=${lng}`
     );
     if (!response.ok) {
@@ -15,4 +16,19 @@ async function fetchSunriseSunsetData(lat, lng) {
   }
 }
 
-export { fetchSunriseSunsetData };
+// Make an API call to https://api.geocodify.com
+async function getCoordinatesFromAddress(address) {
+  // Replace with your Geocodify API endpoint and key
+  const geocodifyApiUrl = `https://api.geocodify.com/v2/geocode?api_key=txmifcC2HNA6iah4cXCbbiYAbtEDKT9Z&q=${encodeURIComponent(
+    address
+  )}`;
+  const response = await fetch(geocodifyApiUrl);
+  const data = await response.json();
+  console.log("API Response:", data);
+  const lat = data.response.features[0].geometry.coordinates[1];
+  const lng = data.response.features[0].geometry.coordinates[0];
+  const locationOutput = data.response.features[0].properties.label;
+  return { lat, lng, locationOutput };
+}
+
+export { fetchSunriseSunsetData, getCoordinatesFromAddress };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { fetchSunriseSunsetData } from "./api.js";
+import { getCoordinatesFromAddress } from "./api.js";
 
 document.getElementById("searchButton").addEventListener("click", () => {
   const address = document.getElementById("addressInput").value;
@@ -21,20 +22,6 @@ document.getElementById("searchButton").addEventListener("click", () => {
       displayError("Error fetching location coordinates.");
     });
 });
-
-async function getCoordinatesFromAddress(address) {
-  // Replace with your Geocodify API endpoint and key
-  const geocodifyApiUrl = `https://api.geocodify.com/v2/geocode?api_key=txmifcC2HNA6iah4cXCbbiYAbtEDKT9Z&q=${encodeURIComponent(
-    address
-  )}`;
-  const response = await fetch(geocodifyApiUrl);
-  const data = await response.json();
-  console.log("API Response:", data);
-  const lat = data.response.features[0].geometry.coordinates[1];
-  const lng = data.response.features[0].geometry.coordinates[0];
-  const locationOutput = data.response.features[0].properties.label;
-  return { lat, lng, locationOutput };
-}
 
 function displaySunriseSunsetData(data) {
   if (data.results) {
