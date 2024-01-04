d0f207ce0a46e887a808cf2c61606ea9678f77a7
Edreyel Adora <edreyel.aadora@gmail.com>
2024-01-04 10:03:28 +0800

    refactor: add try-catch block to the api call

--- js/api/geocoding.js
+++ js/api/geocoding.js
@@ -1,14 +1,27 @@
 // Make an API call to https://api.geocodify.com
+// TODO: add more error handling
 export async function getCoordinatesFromAddress(address) {
-    // Replace with your Geocodify API endpoint and key
-    const geocodifyApiUrl = `https://api.geocodify.com/v2/geocode?api_key=txmifcC2HNA6iah4cXCbbiYAbtEDKT9Z&q=${encodeURIComponent(
-      address
-    )}`;
-    const response = await fetch(geocodifyApiUrl);
-    const data = await response.json();
-    console.log("API Response:", data);
-    const lat = data.response.features[0].geometry.coordinates[1];
-    const lng = data.response.features[0].geometry.coordinates[0];
-    const locationOutput = data.response.features[0].properties.label;
-    return { lat, lng, locationOutput };
+  try {
+    const response = await fetch(
+      // Replace with your Geocodify API key
+      `https://api.geocodify.com/v2/geocode?api_key=txmifcC2HNA6iah4cXCbbiYAbtEDKT9Z&q=${encodeURIComponent(
+        address
+      )}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      // Get data from the response and coordinates from the data
+      const data = await response.json();
+      const coordinates = data.response.features[0].geometry.coordinates;
+      // Return the coordinates and location output
+      return {
+        lat: coordinates[1],
+        lng: coordinates[0],
+        locationOutput: data.response.features[0].properties.label,
+      };
+    }
+  } catch (error) {
+    console.error("Error fetching coordinates:", error);
   }
+}
